fd2d0cdc27a5e26a1ce4281c632987925a242fea
garynye <72671979+garynye@users.noreply.github.com>
2020-10-31 23:04:13 -0500

    advanced tut

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // console.log(useState('hello'))
+  // const value = useState('wofij')[0]
+  // const handler = useState('feiuf')[1]
+  // console.log(value,handler)
+  const [apple, setApple] = useState("hello");
+  const handleClick = () => {
+
+    if (apple === 'Hello') {
+      setApple("Hello World");
+    } else {
+      setApple("Hello");
+    }
+
+
+  };
+  return (
+    <React.Fragment>
+      <h1>{apple}</h1>
+      <button className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
+  //you can import, or use the react function and the use state.  if you just use once, don't bother importing usestate  
+  const [people, setPeople] = React.useState([])
   return <h2>useState array example</h2>;
 };
 
